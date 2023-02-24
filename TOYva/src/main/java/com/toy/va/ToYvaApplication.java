12a4a6a2b0fd919025d1fcab5b62b4f22c803574
thusis <102267039+thusis@users.noreply.github.com>
2023-02-25 00:25:39 +0900

    feat : SPRING 간단한 카페 구현하기 설정파일

---
+++ TOYva/src/main/java/com/toy/va/ToYvaApplication.java
@@ -0,0 +1,13 @@
+package com.toy.va;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToYvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToYvaApplication.class, args);
+	}
+
+}
