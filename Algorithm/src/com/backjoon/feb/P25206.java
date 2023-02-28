2b0adb4c5d5edd466cdfcb086bb1e6a863174fb9
thusis <102267039+thusis@users.noreply.github.com>
2023-03-01 01:17:58 +0900

    [ALG]P25206너의평점은
    (학점x평점)의 합 / 학점의 총합 으로 평균학점 구하는 단순한 문제. BufferedReader 와 switch 구문을 연습하기
    좋았음

--- Algorithm/src/com/backjoon/feb/P25206.java
+++ Algorithm/src/com/backjoon/feb/P25206.java
@@ -3,10 +3,9 @@ package com.backjoon.feb;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class P25206 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		/***
 		 * 학점: a
 		 * 과목평점: b
@@ -14,77 +13,53 @@ public class P25206 {
 		 * 총 과목 개수
 		 * 이 때 전공평점이 3.3 이상이어야 한다
 		 */
-		double a = 0;
-		double b = 0;
-//		double c = 0;
-		int d = 20;
+		
+		double a = 0; //학점의 총합 = 분모
+		double b = 0; //개별 학점
+		double c = 0; // 과목평점
+		double bunja = 0;
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input;
-		while((input = br.readLine())!=null) {
-			String[] strArr = input.split(" ");
-			for(int i=0; i<strArr.length; i++) {
-				if(i%3==1) { // 학점
-					a = a + Double.parseDouble(strArr[i]);
-				}else if(i%3==2) { // 과목평점
-					switch(strArr[i]) {
-					case "A+": 
-						b = b + 4.5;
-						break;
-					case "A0": 
-						b = b + 4.0;
-						break;
-					case "B+": 
-						b = b + 3.5;
-						break;
-					case "B0": 
-						b = b + 3.0;
-						break;
-					case "C+": 
-						b = b + 2.5;
-						break;
-					case "C0": 
-						b = b + 2.0;
-						break;
-					case "D+": 
-						b = b + 1.5;
-						break;
-					case "D0": 
-						b = b + 1.0;
-						break;
-					case "F": 
-						break;
-					default: // 등급이 P인 과목은 계산에서 제외해야 한다.
-						a = a - Double.parseDouble(strArr[i-1]);
-						d = d - 1;
-					}
+		int n = 1;
+		try {
+			while((input = br.readLine())!=null) {
+//				System.out.println("\n"+n+"번째 input: "+input);
+				String[] strArr = input.split(" ");
+				b = Double.parseDouble(strArr[1]);
+				a = a + b;
+
+				switch(strArr[2]) {
+				case "A+": c = 4.5; break;
+				case "A0": c = 4.0;	break;
+				case "B+": c = 3.5; break;
+				case "B0": c = 3.0; break;
+				case "C+": c = 2.5;	break;
+				case "C0": c = 2.0;	break;
+				case "D+": c = 1.5; break;
+				case "D0": c = 1.0; break;
+				case "F": c = 0.0;	break;
+				default: // 등급이 P인 과목은 계산에서 제외해야 한다.
+					a = a - b;
+					c = 0.0;
 				}
+				bunja = bunja + b*c;
+				
+//				System.out.println(n+"번째 누적학점: " + a);
+//				System.out.println(n+"번째 개별학점: " + b);
+//				System.out.println(n+"번째 과목평점: " + c);
+//				System.out.println(n+"번째 분자: " + bunja);
+//				System.out.println(n+"번째 전공평점: " + bunja/a);
+				if(n==20) {
+					System.out.println(bunja/a);
+				}
+				n++;
 			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-//		String[] strArr = usingStringTokenizer(input);
-//		String[] strArrB = usingsplit(input);
-		
-		System.out.println("누적학점: " + a);
-		System.out.println("누적과목평점: " + a);
-		System.out.println("P를 제외한 총 과목수: " + d);
 	}
 
-	private static String[] usingsplit(String input) {
-		return input.split(" ");
-	}
-
-	private static String[] usingStringTokenizer(String input) {
-		StringTokenizer st = new StringTokenizer(input);
-		
-		System.out.println("총 토큰 갯수:"+st.countTokens());
-		String[] strArr = new String[st.countTokens()];
-		int i = 0;
-		while(st.hasMoreTokens()) {
-			strArr[i] = st.nextToken();
-			i++;
-		}
-		
-		return strArr;
-	}
 }
