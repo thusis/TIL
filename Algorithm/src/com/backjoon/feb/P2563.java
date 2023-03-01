3f88bf8a65c2a0572dee58a3436b64757340337f
thusis <102267039+thusis@users.noreply.github.com>
2023-03-01 23:10:44 +0900

    [ALG] P2869 달팽이는 올라가고 싶다
    BufferedReader 를 이용해서 제한 시간 내에 문제 풀어야함. for 문 사용하지 않고 수식으로 풀 것

--- Algorithm/src/com/backjoon/feb/P2563.java
+++ Algorithm/src/com/backjoon/feb/P2563.java
@@ -23,7 +23,6 @@ public class P2563 {
 			int[] yArr = new int[n];
 			
 			for(int i=0; i<n; i++) {
-				int minus = 0;
 				String input = br.readLine();
 				xArr[i] = Integer.parseInt(input.split(" ")[0]);
 				yArr[i] = Integer.parseInt(input.split(" ")[1]);
@@ -31,17 +30,34 @@ public class P2563 {
 				/** 이미 있는 것들(xArr[j])과 비교를 한다면 새로운 값(xArr[i])이 추가될 때마다 for문이 돌아가고 바로바로 겹치는 부분을 빼야 함
 				 * 겹치는 부분의 경우 : (10 - (x축 두 수의 차의 절대값)) * (10 - (y축 두 수의 차의 절대값))  
 				 */
+				
+				System.out.println(i+"번째 배열 : ("+xArr[i]+","+yArr[i]+")");
+				
 				if(i>0) {
+					
+					int minus = 0; // 뺄 값
+					
 					for(int j=0; j<i; j++) {
-						if((xArr[i]-10< xArr[j] && xArr[j]<xArr[i]+10) 
-								&& (yArr[i]-10< yArr[j] && yArr[j]<yArr[i]+10)) {
-							minus = (10-Math.abs(xArr[i]-xArr[j]))*(10-Math.abs(xArr[i]-xArr[j]));
-							result = result - minus;
+						int xMinReplace = xArr[i]-10;
+						int yMinReplace = yArr[i]-10;
+						if(xArr[i]<10) {xMinReplace = 0;}
+						if(yArr[i]<10) {yMinReplace = 0;}
+						
+						if((xMinReplace< xArr[j] && xArr[j]<xArr[i]+10) //조건1-1
+								&& (yMinReplace< yArr[j] && yArr[j]<yArr[i]+10)) { //조건2-1
+							minus = minus + (10-Math.abs(xArr[i]-xArr[j]))*(10-Math.abs(yArr[i]-yArr[j])); 
+							System.out.println(i+"번째 input을 "+j+" 번째 값("+xArr[j]+","+yArr[j]+")이랑 비교했을 때 뺄 값:" +minus);
+						}
+						if(minus>=100) {
+							minus = 100;
+							break;
 						}
 					}
+					result = result - minus;
 				}
-				System.out.println(result);
+				
 			}
+			System.out.println(result);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
---
+++ Algorithm/src/com/backjoon/mar/P2869.java
@@ -0,0 +1,72 @@
+package com.backjoon.mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2869 {
+	//달팽이는 올라가고 싶다 
+	public static void main(String[] args) {
+		/**
+		 * 시간 제한 0.25초
+		 * A, B, V (1 ≤ B < A ≤ V ≤ 1,000,000,000)
+		 * A 낮에 올라감
+		 * B 밤에 내려감
+		 * V 높이
+		 */
+//		tryFirst();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			int v = Integer.parseInt(input[2]);
+			
+			if(v<a) {
+				System.out.println(1);
+				return;
+//			} else if ( v<=(v-a)*(a-b)+a ) {
+			} else if ( (v>=a) && (b!=a-1) ) {
+				System.out.println(v-a+1);
+			} else {
+				System.out.println(Math.ceil(v/(a-b)));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	private static void tryFirst() {
+//		System.out.println(Math.ceil(1.0)); // 1.0
+//		System.out.println(Math.ceil(1.5)); // 2.0
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] input = br.readLine().split(" ");
+			long x = Long.parseLong(input[0]) - Long.parseLong(input[1]);
+			long v = Long.parseLong(input[2]);
+			
+			long day = v / x;
+			
+			System.out.println("하루에 "+x+"만큼 올라갈 때 "+v+"까지 올라가려면 "+day+"만큼 걸린다");
+			
+			
+			
+			if(v < Long.parseLong(input[0])) {
+				System.out.println(1);
+				return;
+			} else if ( v < x*Math.floor(day-1)+Long.parseLong(input[0])) {
+				System.out.println(Math.ceil(day-1));
+			} else {
+				System.out.println(Math.ceil(day));
+			}
+			
+		/* 1 만약 v < a 라면 output = 1
+		 * 2 만약 x * Math.floor(day-1) + a > v 라면 output = Math.floor(day-1)
+		 * 3 그 외의 경우 output =Math.floor(day)
+		 */
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
