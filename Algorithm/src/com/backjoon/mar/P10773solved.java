44c49c5195267bb348fe367c57d8a22a7f880f00
thusis <102267039+thusis@users.noreply.github.com>
2023-03-02 16:54:16 +0900

    [ALG] P10773 제로
    Stack을 이용해서 수의 합 구하기

---
+++ Algorithm/src/com/backjoon/mar/P10773solved.java
@@ -0,0 +1,45 @@
+package com.backjoon.mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class P10773solved {
+	//제로
+	/**
+	 * 0을 부른 경우 가장 최근에 재민이가 쓴 수가 지워진다 => stack.pop();
+	 * 모든 수의 합을 출력한다  
+	 */
+	
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int k = Integer.parseInt(br.readLine());
+			
+			Stack<Integer> stack = new Stack<>();
+			for(int i = 0; i<k; i++) {
+				int n = Integer.parseInt(br.readLine().split(" ")[0]);
+				if(n==0) {
+					stack.pop();
+				}else {
+					stack.push(n);
+				}
+//				System.out.println(i+"번째 입력값("+n+")에 대한 Stack: "+stack);
+			}
+			int output = 0;
+			while(!stack.empty()) {
+				output = output + stack.pop();
+			}
+			System.out.println(output);
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
