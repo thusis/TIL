ba3ec8279ec3053ebd5998dc72cecc56281fc15b
thusis <102267039+thusis@users.noreply.github.com>
2023-03-04 22:56:08 +0900

    [ALG] P9935 폭발
    스택 사용

---
+++ Algorithm/src/com/backjoon/mar/P9935.java
@@ -0,0 +1,30 @@
+package com.backjoon.mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P9935 {
+	//문자열 폭발
+	/**
+	 * 
+	mirkovC4nizCC44
+	C4
+	
+	12ab112ab2ab
+	12ab
+	 * @param ags
+	 */
+	public static void main(String[] ags) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			String input = br.readLine();
+			String explosion = br.readLine();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
