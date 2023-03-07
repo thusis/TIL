dbc847ccc786d570a3ff6d14f6ba5f9dbe0a9588
thusis <102267039+thusis@users.noreply.github.com>
2023-03-07 17:38:22 +0900

    [ALG] P1037 약수 : String[] to ArrayList<Integer>, Collections.sort()

---
+++ Algorithm/src/com/backjoon/mar/P1037solved.java
@@ -0,0 +1,41 @@
+package com.backjoon.mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class P1037solved {
+//약수
+	public static void main(String[] args) {
+		/**
+		 * N : N은 A의 배수, 
+		 * A : 1이나 N이 아니다 | A는 N의 약수
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> list = new ArrayList<>();
+		try {
+			int number = Integer.parseInt(br.readLine());
+			String[] strs = br.readLine().split(" ");
+			
+			for(String str : strs) {
+				list.add(Integer.parseInt(str));
+			}
+			
+			Collections.sort(list);
+			
+			int output = list.get(0)*list.get(number-1);
+			System.out.println(output);
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
