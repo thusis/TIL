8728125828a47ccb929dc3e0deb4096e55874f83
thusis <102267039+thusis@users.noreply.github.com>
2023-03-09 17:45:41 +0900

    [ALG] P17427 약수의 합2 (틀림)
    try2

--- Algorithm/src/com/backjoon/mar/P17427.java
+++ Algorithm/src/com/backjoon/mar/P17427.java
@@ -23,11 +23,12 @@ public class P17427 {
 			
 			int ga = 0;
 			
-			for(int i=1; i<=n; i++) {
-				HashSet<Integer> set = getYaksu(i);
-				int fa = getYaksuHab(set);
+			for(int i=1; i<=n; i++) { // 시간복잡도 : O(sqrt(n)) * n
+//				HashSet<Integer> set = getYaksu(i);
+//				int fa = getYaksuHab(set);
+				int fa = getYaksuHab(i);
 				ga = ga + fa;
-				System.out.println(i+"를 합한 값은 "+ga+"\n");
+				System.out.println(i+"의 약수의 합인 "+fa+"를 합한 값은 "+ga+"\n");
 			}
 			
 			System.out.println(ga);
@@ -41,29 +42,16 @@ public class P17427 {
 		
 	}
 
-	private static int getYaksuHab(HashSet<Integer> set) {
+	private static int getYaksuHab(int n) {
 		int result = 0;
-		Iterator<Integer> setIt = set.iterator();
-		while(setIt.hasNext()) {
-			int a = setIt.next();
-			result = result + a;
-			System.out.print(a+" ");
-		}
-		System.out.println(result);
-		return result;
-	}
-
-	private static HashSet<Integer> getYaksu(int n) {
-
-		HashSet<Integer> set = new HashSet<>();
 		
-		for(int i=1; i<=Math.sqrt(n); i++) {
+		for(int i=1; i<=Math.sqrt(n); i++) {//현재 n에 대한 시간 복잡도 O(sqrt(n))
 			if( n % i == 0 ) {
-				set.add(i);
-				set.add(n/i);
+				if(i!=n/i) { result = result  + i + n/i;}
+				else { result = result + i; }
 			}
 		}
-		return set;
+		return result;
 	}
 
 }
