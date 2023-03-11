41473523446f5b288b7f53eaa3a5a7abd12844c7
thusis <102267039+thusis@users.noreply.github.com>
2023-03-11 19:09:40 +0900

    [ALG] P2751 수 정렬하기2 > 병합정렬 이용, BufferedReader+BufferedWriter 사용해야 시간초과x

--- Algorithm/src/com/backjoon/mar/P2751.java
+++ Algorithm/src/com/backjoon/mar/P2751.java
@@ -1,31 +1,96 @@
 package com.backjoon.mar;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.OutputStreamWriter;
 
 public class P2751 {
 //수 정렬하기2
+	
+	private static int[] sorted; 
+	
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		try {
-			int n = Integer.parseInt(br.readLine());
+			int[] arr = new int[Integer.parseInt(br.readLine())];
+			for(int i=0; i<arr.length; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			
+			merge_sort(arr);
+			
+			for(int i=0; i<arr.length; i++) {
+				bw.write(arr[i]+"\n");
+			}
+			bw.close();
+	        br.close();
 
-			ArrayList<Integer> list = new ArrayList();
-			for(int i=0; i<n; i++) {
-				list.add(Integer.parseInt(br.readLine()));
-			}
-			Collections.sort(list);
-			for(int i=0; i<n; i++) {
-				System.out.println(list.get(i));
-			}
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+/**
+ * package com.algorithm.sort;의
+ * MergeSortBottomUp 클래스 참조
+ */
+	private static void merge_sort(int[] arr) {
+		sorted = new int[arr.length];
+		merge_sort(arr, 0, arr.length-1);
+		sorted = null;
+	}
+
+	private static void merge_sort(int[] arr, int start, int end) {
+		for(int size = 1; size<=end; size += size) {
+			for(int l=0; l<= end-size; l+=(2*size)) {
+				int low = l;
+				int mid = l+size-1;
+				int high = Math.min(l+(2*size)-1, end);
+				merge(arr,low,mid,high);
+			}
+		}
+		
+	}
+
+	private static void merge(int[] arr, int left, int mid, int right) {
+		int l = left;
+		int r = mid + 1;
+		int i = left; //인덱스
+		
+		while ( l <= mid && r <= right ) {
+			if(arr[l] <= arr[r]) {
+				sorted[i] = arr[l];
+				i++;
+				l++;
+			} else {
+				sorted[i] = arr[r];
+				i++;
+				r++;
+			}
+		}
+		
+		if(l>mid) {
+			while(r<=right) {
+				sorted[i] = arr[r];
+				i++;
+				r++;
+			}
+		} else {
+			while(l<=mid) {
+				sorted[i] = arr[l];
+				i++;
+				l++;
+			}
+		}
+		
+		for(int j = left; j<=right; j++) {
+			arr[j] = sorted[j];
 		}
 	}
 }
