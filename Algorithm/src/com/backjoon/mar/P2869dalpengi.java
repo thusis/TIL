6cd24a333f9b92e17c83c254c9b6b6466685151f
thusis <102267039+thusis@users.noreply.github.com>
2023-03-14 21:32:49 +0900

    fix

--- Algorithm/src/com/backjoon/mar/P2869dalpengi.java
+++ Algorithm/src/com/backjoon/mar/P2869dalpengi.java
@@ -1,10 +1,12 @@
 package com.backjoon.mar;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class P2869 {
+public class P2869dalpengi {
 	//달팽이는 올라가고 싶다 
 	public static void main(String[] args) {
 		/**
@@ -17,6 +19,8 @@ public class P2869 {
 //		tryFirst();
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		try {
 			String[] input = br.readLine().split(" ");
 			int a = Integer.parseInt(input[0]);
