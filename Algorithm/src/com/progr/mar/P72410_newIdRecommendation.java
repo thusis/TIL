c95826682cecfde91c8fa1525ae6fffe639028e5
thusis <102267039+thusis@users.noreply.github.com>
2023-03-19 23:51:57 +0900

    [ALG] 프로그래머스 - 신규아이디 추천 :  정규표현식 이용
    - 정규표현식을 이용하자(참고:https://zzang9ha.tistory.com/322)
    - String에 쓰이는 메소드를 활용한다

--- Algorithm/src/com/progr/mar/P72410_newIdRecommendation.java
+++ Algorithm/src/com/progr/mar/P72410_newIdRecommendation.java
@@ -7,8 +7,8 @@ public class P72410_newIdRecommendation {
 	}
 	
 	public static String solution(String new_id) {
-		String answer = "";
 		
+		String editingId = "";
 		/**
 		 * 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
 		 * 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
@@ -17,35 +17,55 @@ public class P72410_newIdRecommendation {
 		 * 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
 		 * 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
 		 *      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-		 *      7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-
-		 * 아스키코드
-		 * A-Z 65-90
-		 * a-z 97-122
+		 * 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 		 */
+
+		editingId = new_id.toLowerCase();
+		/**
+		 * 2
+		 * a-z 0-9 \- \_ \. 가 아니면 "" 로 제외
+		 * "[^0-9a-z\\-_.]"
+		 */
+		editingId = editingId.replaceAll("[^0-9a-z\\-_.]", "");
 		
-		String editingId = "";
-		String temp = "";
-		for(int i=0; i<new_id.length(); i++) {
-			//1단계
-			if(new_id.charAt(i)>=65 && new_id.charAt(i)<=90) {
-				temp = (new_id.charAt(i)+"").toLowerCase();
-				editingId = editingId + temp;
-			} else {
-				editingId = editingId + new_id.charAt(i);
-			}
-			
-			/**
-			 * a-z 0-9 \- \_ \. 가 아니면 "" 로 제외
-			 * 
-			 * https://zzang9ha.tistory.com/322 참고
-			 */
-			
-			Pattern p
-			
-			
+		/**
+		 * 3
+		 * \\. (마침표)
+		 * {2,} 2회 이상
+		 */
+		editingId = editingId.replaceAll("\\.{2,}", ".");
+		
+		/**
+		 * 4
+		 * ^ : 문자의 시작
+		 * ^[.] : 문자열이 마침표로 시작하는 것
+		 * | : or
+		 * [.]$ : 마침표로 끝나는 것
+		 */
+		editingId = editingId.replaceAll("^[.]|[,]$", "");
+		
+		/**
+		 * 5
+		 * 
+		 */
+		if(editingId.equals("")) {
+			editingId = "a";
 		}
 		
-		return answer;
+		/**
+		 * 6
+		 * 
+		 */
+		if(editingId.length()>=16) {
+			editingId = editingId.substring(0,15);
+		}
+		editingId = editingId.replaceAll("[.]$", "");
+		
+		String temp = editingId.charAt(editingId.length()-1) + "";
+		while(editingId.length()<=2) {
+			editingId = editingId + temp;
+		}
+		
+		return editingId;
 	}
 }
