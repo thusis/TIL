d90b061ecda0066faf95bd8d117cc377ba0ab65e
thusis <102267039+thusis@users.noreply.github.com>
2023-03-21 13:03:44 +0900

    [ALG] P2677 아파트단지번호붙이기 - DFS, BFS 이용(ing)

---
+++ Algorithm/src/com/backjoon/mar/P2667_numberingDanji.java
@@ -0,0 +1,23 @@
+package com.backjoon.mar;
+
+public class P2667_numberingDanji {
+	//https://www.acmicpc.net/problem/2667 단지번호붙이기 
+	//https://n1tjrgns.tistory.com/245 참고해 DFS, BFS
+	
+	/**
+	 * int n 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)
+	 * int n 각각 N개의 자료(0혹은 1)
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+	 *
+	 */
+		public static void main(String[] args) {
+			
+		}
+}
